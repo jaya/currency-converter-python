91ebcca7a7377a91892c8bb0a4f82b49790c3f60
nutaro <nutaro@protonmail.com>
2025-07-30 11:54:04 -0300

    added migrations

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from service import CurrencyService
 from validators import Convert
 
 app = FastAPI()
@@ -7,4 +8,16 @@ app = FastAPI()
 
 @app.post("/convert/")
 async def convert(data: Convert):
-    return {}
+    service = CurrencyService()
+    transaction = service.calculate_transaction(data)
+    service.persist(transaction)
+    return {
+        "transaction_id": transaction.id,
+        "user_id": transaction.user_id,
+        "from_currency": transaction.from_currency,
+        "to_currency": transaction.to_currency,
+        "from_value": data.value,
+        "to_value": transaction.value,
+        "rate": transaction.rate,
+        "timestamp": transaction.timestamp,
+    }
